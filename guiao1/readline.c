1009165547695df1ae1ee178dd5804e998b1f4f1
luk <ojosepedro@gmail.com>
2019-04-15 15:37:28 +0100

    readline

---
+++ guiao1/readline.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t readln(int fildes, void* buf, ssize_t nbyte){
+  ssize_t size;
+  int res;
+  char byte;
+  char *buffer = (char*)buf; // casting void*
+  while ((res = read(fildes,&byte,1) > 0) && size < nbyte){
+    buffer[size++] = byte;
+    if (byte == '\n')
+      return size;
+  }
+  return size;
+}
