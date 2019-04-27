ee70de107bbe1dfce1a092fcfa652d6a6f31f478
luk <ojosepedro@gmail.com>
2019-04-27 16:37:32 +0100

    ex4

--- guiao2/ex4.c
+++ guiao2/ex4.c
@@ -4,4 +4,18 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
+  pid_t pid;
+  int status;
+  for(int i=0; i<10; i++){
+    if((pid = fork()) == 0){
+      printf("Pid %d\n", getpid());
+      printf("Parent pid %d\n", getppid());
+      _exit(i);
+    }
+  }
+  for(int i=0; i<10; i++){
+    wait(&status);
+    printf("Exit code %d\n", WEXITSTATUS(status));
+  }
+  return 0;
 }
